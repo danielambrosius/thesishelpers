acb551161b2b060b94ffce39bccaf243136af4da
Daniel Prince <danielahprince@live.com>
2020-03-20 15:39:04 +0100

    Wrote raster clipping function

---
+++ src/thesishelpers/raster_tools.py
@@ -0,0 +1,41 @@
+import rasterio
+from rasterio.mask import mask
+import json
+import geopandas as gpd
+
+
+def clip_to_polygon(in_path: str, bounding_gdf: gpd.GeoDataFrame, buffer=None):
+    """clips a raster, saves the clipped raster, opens, and returns
+       a handle to it
+    
+    Arguments:
+        in_path {str} -- Path of original raster
+        bounding_gdf {gpd.GeoDataFrame} -- gdf where the first line is the bounding geometry
+    """
+    if buffer:
+        bounding_gdf = bounding_gdf.buffer(buffer)
+
+    def get_features(gdf):
+    # Function to parse features from GeoDataFrame in such a manner that rasterio wants them
+        return [json.loads(gdf.to_json())['features'][0]['geometry']]
+
+    geometry = get_features(bounding_gdf)
+
+    with rasterio.open(in_path) as src:
+        out_image, out_transform = rasterio.mask.mask(src, geometry, crop=True)
+        out_meta = src.meta
+
+    out_meta.update(
+        {
+            "driver": "GTiff",
+            "height": out_image.shape[1],
+            "width": out_image.shape[2],
+            "transform": out_transform,
+        }
+    )
+
+    out_path = in_path.split(".tif")[0] + "_clipped.tif"
+    with rasterio.open(out_path, "w", **out_meta) as dest:
+        dest.write(out_image)
+
+    return rasterio.open(out_path)
